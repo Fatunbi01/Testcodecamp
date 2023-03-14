53f411fd8e5574b5f10d6eec1367ad0860de3e7e
Fatunbi01 <yfatunbi@gmail.com>
2023-03-14 20:00:05 +0100

    introduction to java project

---
+++ FirstJavaProject/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("I am a QA Engineer");
+        System.out.println("I dedicate a high level of energy and commitment to ensuring quality product");
+        System.out.println("I enjoy seeing quality applications on production/live environment");
+        System.out.println("I enjoy finding bugs,reporting bugs and providing possible solutions");
+        System.out.println("My mission and vision is to make users happy by delivering high quality products");
+    }
+}
